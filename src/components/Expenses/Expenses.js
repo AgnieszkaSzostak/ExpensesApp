17e516e12712e0be627445a60392b9cc437bb84c
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2022-03-15 21:27:31 +0100

    Add ExpensesList component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,25 @@
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter'; 
 import React, {useState} from 'react';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
     const [choosedOption, setChoosedOption] = useState('2020');
-
-
+    
     const saveDateHandler = (enteredDate) => {
        setChoosedOption(enteredDate);
     };
-
+    
     const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === choosedOption);
-
+    
     return(
         <div>
             <Card className="expenses">
                 <ExpensesFilter 
                 onSaveDate={saveDateHandler} 
                 initialDate={choosedOption} />
-                {filteredExpenses.map(expense => 
-                    <ExpenseItem  
-                        key={expense.id} 
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date}
-                    />)}
-                
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>  
     );
